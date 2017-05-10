690b69ed1a2d6e58319efed58d781af98e6eeb30
mikeporter <porter.michael.james@gmail.com>
2017-05-09 20:38:27 -0700

    basic layout, beginning some constructor functionality

---
+++ scripts/article.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var articles = [];
+
+function Article (allArticlesObj) {
+  this.title = allArticlesObj.title
+  this.githubUrl = allArticlesObj.githubUrl
+  this.initialDate = allArticlesObj.initialDate
+  this.lastEditedDate = allArticlesObj.lastEditedDate
+  this.category = allArticlesObj.category
+  this.articleContent = allArticlesObj.articleContent
+};
+
+Article.prototype.toHtml = function(){
+  var $newArticle = $('.template').clone();
+  $newArticle.find('.template h3').html('frog');
+  $newArticle.find('.article-body').html(this.articleContent)
+
+  return $newArticle
+};
+
+allArticles.forEach(function(articleObj) {
+  articles.push(new Article(articleObj));
+};)
+
+articles.forEach(function(article){
+  $('#template').append(article.toHtml);
+});
