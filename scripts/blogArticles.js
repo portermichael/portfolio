677f8c5c581334b313d3794b0ff0b9b73794b319
mikeporter <porter.michael.james@gmail.com>
2017-05-10 20:49:49 -0700

    added a constructor object that populates data

--- scripts/blogArticles.js
+++ scripts/blogArticles.js
@@ -2,43 +2,43 @@
 
 var allArticles = [
   {
-    title = "What is it?",
-    linkedUrl = "",
-    initialDate = "",
-    lastEditedDate = "",
-    category = "Github",
-    articleContent = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    title: "What is it?",
+    linkedUrl: "https://www.github.com",
+    createdDate: "2015-10-05",
+    lastEditedDate: "2015-11-05",
+    category: "Github",
+    articleContent: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
   },
   {
-    title = "Why do we use it?",
-    githubUrl = "",
-    initialDate = "",
-    lastEditedDate = "",
-    category = "Github",
-    articleContent = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+    title: "Why do we use it?",
+    githubUrl: "https://www.github.com",
+    createdDate: "2015-10-04",
+    lastEditedDate: "2015-11-04",
+    category: "Github",
+    articleContent: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
   },
   {
-    title = "Where does it come from?",
-    githubUrl = "",
-    initialDate = "",
-    lastEditedDate = "",
-    category = "Blog",
-    articleContent = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32."
+    title: "Where does it come from?",
+    githubUrl: "",
+    createdDate: "2015-10-02",
+    lastEditedDate: "2015-11-06",
+    category: "Blog",
+    articleContent: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32."
   },
   {
-    title = "Where can I get some?",
-    githubUrl = "",
-    initialDate = "",
-    lastEditedDate = "",
-    category = "Github",
-    articleContent = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
+    title: "Where can I get some?",
+    githubUrl: "https://www.github.com",
+    createdDate: "2015-10-02",
+    lastEditedDate: "2015-11-02",
+    category: "Github",
+    articleContent: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
   },
   {
-    title = "Lorem ipsum",
-    githubUrl = "",
-    initialDate = "",
-    lastEditedDate = "",
-    category = "Blog",
-    articleContent = "orem ipsum dolor sit amet, consectetur adipiscing elit. Etiam suscipit magna ac enim viverra, sit amet congue nulla lacinia. Suspendisse iaculis massa elit, eu facilisis quam malesuada et. Aliquam posuere laoreet nulla, ac dictum ex cursus ut. Nullam ut dolor sit amet libero pellentesque pellentesque et eu nunc. Vivamus vitae finibus massa, quis volutpat nunc. Vivamus aliquet ut arcu sed rhoncus. Aliquam dictum id ligula non accumsan. Donec sed sem eget velit suscipit viverra non quis dolor. Proin eu posuere magna. Nam ut diam eros."
+    title: "Lorem ipsum",
+    githubUrl: "",
+    createdDate: "2015-10-05",
+    lastEditedDate: "2015-11-01",
+    category: "Blog",
+    articleContent: "orem ipsum dolor sit amet, consectetur adipiscing elit. Etiam suscipit magna ac enim viverra, sit amet congue nulla lacinia. Suspendisse iaculis massa elit, eu facilisis quam malesuada et. Aliquam posuere laoreet nulla, ac dictum ex cursus ut. Nullam ut dolor sit amet libero pellentesque pellentesque et eu nunc. Vivamus vitae finibus massa, quis volutpat nunc. Vivamus aliquet ut arcu sed rhoncus. Aliquam dictum id ligula non accumsan. Donec sed sem eget velit suscipit viverra non quis dolor. Proin eu posuere magna. Nam ut diam eros."
   }
 ]
