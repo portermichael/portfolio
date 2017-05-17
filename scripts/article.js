614a87823b033e1c4e323417d16b1513a4d83c2d
mikeporter <porter.michael.james@gmail.com>
2017-05-16 18:06:45 -0700

    added json file and ajax call

--- scripts/article.js
+++ scripts/article.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var articles = [];
+Article.articles = [];
 
 function Article (allArticlesObj) {
   this.title = allArticlesObj.title
@@ -12,8 +12,8 @@ function Article (allArticlesObj) {
 }
 
 Article.prototype.toHtml = function() {
-  var articleTemplate = $('#articleTemplate').html();
-  var compileArticle = Handlebars.compile(articleTemplate);
+  let articleTemplate = $('#articleTemplate').html();
+  let compileArticle = Handlebars.compile(articleTemplate);
 
   this.daysAgo = parseInt(Math.floor((new Date() - new Date(this.createdDate))/60/60/24/1000));
   this.publishStatus = this.createdDate ? `published ${this.daysAgo} days ago` : '(draft)';
@@ -21,14 +21,42 @@ Article.prototype.toHtml = function() {
   return compileArticle(this);
 };
 
-allArticles.sort(function(a, b){
-  return (new Date(b.lastEditedDate) - new Date (a.lastEditedDate));
-});
+Article.loadAll = function(allArticles){
+  allArticles.sort(function(a, b){
+    return (new Date(b.lastEditedDate) - new Date (a.lastEditedDate));
+  });
 
-allArticles.forEach(function(articleObj) {
-  articles.push(new Article(articleObj));
-});
+  allArticles.forEach(function(articleObj) {
+    articles.push(new Article(articleObj));
+  });
+};
 
-articles.forEach(function(article){
+Article.fetchAll = function() {
+  $.ajax(
+    url: '/data/blogArticles.json',
+    method: 'HEAD',
+    success: function(data, message, xhr) {
+      eTag = xhr.getResponseHeader('ETag');
+      if (localStorage.eTag === eTag){
+        Article.loadAll(JSON.parse(localStorage.allArticles))
+        //load tohtml?
+      } else {
+        $.getJSON('data/blogArticles.json').then(function(){
+          localStorage.allArticles = JSON.stringify(data);
+          localStorage.eTag = eTag;
+          Article.loadAll(data);
+          //load tohtml?
+        }, function(err) {
+          console.error(err);
+        });
+      }
+    };
+    error: function(){
+      console.log('An error has occurred.')
+    }
+  )
+}
+
+Article.articles.forEach(function(article){
   $('#articles').append(article.toHtml());
 });
